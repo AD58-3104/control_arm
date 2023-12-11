9fbf42d16652a4c30f8262f4e2601c99ddf50e29
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2023-12-11 17:30:26 +0900

    2軸でのアームの順運動学OK。3次元プロットも試し

--- matlab/plot_arm.m
+++ matlab/plot_arm.m
@@ -1,47 +1,43 @@
 % syms theta
-import Robotics
+% import Robotics
 
 clear;
 
 % オイラー角 Z Y X 
 eul = [0 pi/2 pi/2];
-R = eul2rotm(eul)
+R = eul2rotm(eul);
 
 
-origin_para_theta = pi/2;
-%  Ro = [cos(origin_para_theta) -sin(origin_para_theta) 0;sin(origin_para_theta) cos(origin_para_theta) 0;0 0 1;];
+origin_para_theta = pi/3;
 Ro = rotm2tform(eul2rotm([origin_para_theta 0 0]));
 To = trvec2tform([0 0 0]);
 
-link0_para_a = 2;
-link0_para_theta = pi/2;
- % R0 = [cos(link0_para_theta) -sin(link0_para_theta) 0;sin(link0_para_theta) cos(link0_para_theta) 0;0 0 1;];
+link0_para_a = 3;
+link0_para_theta = -pi/2;
 R0 = rotm2tform(eul2rotm([link0_para_theta 0 0]));
 T0 = trvec2tform([link0_para_a 0 0]);
 
 link1_para_a = 2;
-link1_para_theta = 0;
- % R1 = [cos(link1_para_theta) -sin(link1_para_theta) 0;sin(link1_para_theta) cos(link1_para_theta) 0;0 0 1;];
-% T1 = [R1 [link1_para_a; 0; 0]; 0 0 0 1]
+link1_para_theta = pi;
 R1 = rotm2tform(eul2rotm([link1_para_theta 0 0]));
 T1 = trvec2tform([link1_para_a 0 0]);
 
-res = To * T0 * T1
+link0  = To * Ro * T0 * R0
+link1  = link0 * T1 * R1
 
 % 前がx後ろがy,最後がz
-origin = [0 0 0];
-link0 = [2 0 0];
-link1 = [2 0 0];
-
-result = [origin;link0;link1]
-
-result_x = result(: ,1);
-result_y = result(: ,2);
+result_0 = tform2trvec(link0)
+result_1 = tform2trvec(link1)
+result_x = [0 result_0(1) result_1(1)]
+result_y = [0 result_0(2) result_1(2)]
+result_z = [0 result_0(3) result_1(3)]
 
 % プロットするとこ
-plot(result_x,result_y, '-o');
+grid on;
+plot3(result_x,result_y,result_z, '-o');
 title('My Plot');
 xlabel('x');
 ylabel('y');
+zlabel('z');
 xlim([0 10]);
-ylim([0 10]);
+ylim([-5 10]);
