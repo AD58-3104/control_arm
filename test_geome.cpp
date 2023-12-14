3ef38f2290aab0c9f04fb0ebdc093f29f578a941
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2023-12-15 02:40:12 +0900

    refactor

--- test_geome.cpp
+++ test_geome.cpp
@@ -8,8 +8,8 @@ int main(int argc, char const *argv[])
 {
     Vector3d link_rotate(0, 0, M_PI / 5);
     auto mat = get3DRotationMatrix(link_rotate);
-    StraightChainRobotModel origin(0, 0, Vector3d::Zero(), 0, link_rotate);
-    StraightChainRobotModel link0(1, 3, Vector3d::Zero(), 0, link_rotate);
+    StraightChainRobotModel origin(0, 0, Vector3d::Zero(), 0, Vector3d(0,0,M_PI/3));
+    StraightChainRobotModel link0(1, 3, Vector3d::Zero(), 0, Vector3d(0,0,-M_PI/1.5f));
     StraightChainRobotModel link1(1, 3, Vector3d::Zero(), 0, link_rotate);
     // robot_model.printLinkState();
 
@@ -30,26 +30,14 @@ int main(int argc, char const *argv[])
     result_y.push_back(res(1, 3));
 
     Plot2D plot;
-    // Set the legend
     plot.legend().hide();
-
-    // Set the x and y labels
     plot.xlabel("x");
     plot.ylabel("y");
-
-    // Set the y range
     plot.yrange(-3, 7);
     plot.xrange(0.0f, 7);
-
-    plot.drawWithVecs("lines", result_x, result_y).lineColor("red");
-
-    // Create figure to hold plot
+    plot.drawWithVecs("linespoints", result_x, result_y).lineColor("red");
     Figure fig = {{plot}};
-
-    // Create canvas to hold figure
     Canvas canvas = {{fig}};
-
-    // Show the plot in a pop-up window
     canvas.show();
     return 0;
 }
