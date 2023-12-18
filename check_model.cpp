a444a412d487194ca4abae2bc4c50e88b3d615f6
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2023-12-19 01:15:12 +0900

    update check_model

--- check_model.cpp
+++ check_model.cpp
@@ -9,18 +9,17 @@ int main(int argc, char const *argv[])
     plot2darm();
     ptr->at(2).setJointAngle(M_PI/8);
     plot2darm();
-    ptr->at(3).setJointAngle(M_PI/4);
+    ptr->at(3).setJointAngle(M_PI/8);
     plot2darm();
-    ptr->at(4).setJointAngle(M_PI/4);
+    ptr->at(4).setJointAngle(M_PI/8);
     plot2darm();
     auto res = calcForwardKinematics(); 
-    plotArm(res);
     return 0;
 }
 
 /**
  * 回転方向メモ
- * id 1 =      id1は一番最初のx軸回転
+ * id 1 = 下     id1は一番最初のx軸回転
  * id 2 = 上
  * id 3 = 下
  * id 4 = 下
