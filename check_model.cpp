01c2432aefa78d9f9673778899c2ac325bfd2bf7
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2023-12-19 02:48:28 +0900

    trest

--- check_model.cpp
+++ check_model.cpp
@@ -1,26 +1,47 @@
 #include "robot_arm.hpp"
 
+void printContainer(const std::vector<double>& vec,std::string message = ""){
+    std::cout << message;
+    for  (const auto& v : vec)
+    {
+        std::cout << v << ", ";
+    }
+    std::cout << std::endl;
+}
+
 int main(int argc, char const *argv[])
 {
     //PC上の処理では+だと上に回転する。
     auto ptr = createRobotArm();
     // ptr->at(1).setJointAngle(M_PI/4);
-    ptr->at(1).setJointAngle(M_PI/8);
-    plot2darm();
-    ptr->at(2).setJointAngle(M_PI/8);
-    plot2darm();
-    ptr->at(3).setJointAngle(M_PI/8);
-    plot2darm();
-    ptr->at(4).setJointAngle(M_PI/8);
-    plot2darm();
-    auto res = calcForwardKinematics(); 
+    ptr->at(1).setJointAngle(M_PI/8.0f);
+    plot2darm(true);
+    // ptr = createRobotArm();
+    ptr->at(2).setJointAngle(M_PI/8.0f);
+    auto degs = getJointAngleParam();
+    printContainer(degs,"[joint param degs] ");
+    plot2darm(true);
+    // ptr = createRobotArm();
+    ptr->at(3).setJointAngle(M_PI/8.0f);
+    plot2darm(true);
+    // ptr = createRobotArm();
+    ptr->at(4).setJointAngle(M_PI/8.0f);
+    plot2darm(true);
+    auto res = calcForwardKinematics();
+    degs = getJointAngleParam();
+    printContainer(degs,"[joint param degs] ");
     return 0;
 }
 
 /**
  * 回転方向メモ
- * id 1 = 下     id1は一番最初のx軸回転
- * id 2 = 上
- * id 3 = 下
+ * id 1 = そもそも軸が違うので今回関係なし
+ * id 2 = 下     id2は一番最初のx軸回転
+ * id 3 = 上     ここは恐らく回転方向は同じ。90度が足せていないのが原因...
  * id 4 = 下
+ * id 5 = ? //不明
+ * 
+ * もう一つの問題！！！
+ *  setJointAngleは絶対角度指定だが、CCDでは相対角度が指令になっている。ここを変えないとマズイ
+ * この時リバース設定は結構難しいぞどうしよう...
  */
